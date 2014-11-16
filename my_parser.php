d1ceffb87f38f39b044096e840d789b948474c32
Van Doan <vdoan09@gmail.com>
2014-11-15 23:42:03 -0500

    peakaboo box

---
+++ my_parser.php
@@ -0,0 +1,8 @@
+<?php
+if(isset($_POST['firstname'])){
+    echo $_POST['firstname']."<br>";
+    echo $_POST['lastname']."<br>";
+    echo $_POST['gender']."<br>";
+    echo $_POST['country'];
+}
+?>
